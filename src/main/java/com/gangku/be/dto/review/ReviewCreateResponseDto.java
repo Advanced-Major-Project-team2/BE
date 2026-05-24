7dfc089dd44850399e9e8f904e22deddf3c5b3a9
seeechan <sechan020830@gmail.com>
2026-05-24 20:25:55 +0900

    feat: add review APIs

---
+++ src/main/java/com/gangku/be/dto/review/ReviewCreateResponseDto.java
@@ -0,0 +1,36 @@
+package com.gangku.be.dto.review;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.gangku.be.constant.id.ResourceType;
+import com.gangku.be.domain.Review;
+import com.gangku.be.model.common.PrefixedId;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class ReviewCreateResponseDto {
+    private String reviewId;
+    private Integer rating;
+    private String comment;
+
+    @JsonFormat(
+            shape = JsonFormat.Shape.STRING,
+            pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",
+            timezone = "UTC")
+    private LocalDateTime createdAt;
+
+    public static ReviewCreateResponseDto from(Review review) {
+        String publicReviewId = PrefixedId.of(ResourceType.REVIEW, review.getId()).toExternal();
+
+        return ReviewCreateResponseDto.builder()
+                .reviewId(publicReviewId)
+                .rating(review.getRating())
+                .comment(review.getContent())
+                .createdAt(review.getCreatedAt())
+                .build();
+    }
+}
