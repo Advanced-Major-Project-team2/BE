423e9bd46bb3352c25449cd85a84907c3d12693d
seeechan <sechan020830@gmail.com>
2026-05-24 20:24:55 +0900

    feat: initialize spring boot backend

---
+++ src/main/java/com/gangku/be/BeApplication.java
@@ -0,0 +1,16 @@
+package com.gangku.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/gangku/be/BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gangku.be;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Tag("integration")
+@SpringBootTest
+class BeApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
